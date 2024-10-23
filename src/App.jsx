43f9155cf7a11294e808e1c1e6f65165f4ae7d35
Bablumahto <bablumht18@gmail.com>
2024-10-23 12:26:09 +0530

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold '>Hello World</h1>
-    </div>
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+    </Routes>
+    <Footer/>
+    </>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1>hpme</h1>
+    </div>
+  )
+}
+
+export default Home
